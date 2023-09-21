5b0a05638f2b5de4f4910ba5ed84709389e9d205
AndresGiron <andres.giron.perez@correounivalle.edu.co>
2023-09-20 21:26:00 -0500

    eslintrc correcciones

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,14 @@ module.exports = {
       // Otras reglas...
   
       // Desactivar advertencias (warnings) y solo mostrar errores (errors)
-      "no-warning-comments": "error",
-      "no-console": "error",
-      "no-unused-vars": "error",
+      "no-warning-comments": "off",
+      "no-console": "off",
+      "no-unused-vars": "off",
       "react-hooks/exhaustive-deps": "off",
       // Añade otras reglas aquí si es necesario...
   
       // Ejemplo: Evitar punto y coma al final de las sentencias
-      "semi": "error",
+      "semi": "off",
     },
   };
   
